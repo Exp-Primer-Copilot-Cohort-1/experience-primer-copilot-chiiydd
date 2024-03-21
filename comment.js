b3259c1e3d0b8047cc08add30ab9277d22d52f06
chiiydd <879723824@qq.com>
2024-03-21 11:14:56 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,61 +1,30 @@
 // Create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-var app = http.createServer(function(request, response){
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathName = url.parse(_url, true).pathname;
+// Parse incoming request with JSON
+app.use(express.json());
 
-  if(pathName === '/'){
-    if(queryData.id === undefined){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    } else {
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var sanitizedTitle = sanitizeHtml(title);
-          var sanitizedDescription = sanitizeHtml(description, {
-            allowedTags: ['h1']
-          });
-          var list = template.list(filelist);
-          var html = template.HTML(sanitizedTitle, list,
-            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-            `<a href="/create">create</a>
-            <a href="/update?id=${sanitizedTitle}">update</a>
-            <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
-      });
-    }
-  } else if(pathName === '/create'){
-    fs.readdir('./data', function(error, filelist){
-      var title = 'WEB - create';
-      var list = template.list(filelist);
-      var html = template.HTML(title, list, `
-        <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p
+// Create a variable to store comments
+const comments = [];
+
+// Create a route to get comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+// Create a route to post comments
+app.post('/comments', (req, res) => {
+    const comment = req.body;
+    comments.push(comment);
+    res.json(comment);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
+
 
 
