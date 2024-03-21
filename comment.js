6fa89caa575cc156fe4840606fb3ac953845842e
chiiydd <879723824@qq.com>
2024-03-21 11:23:16 +0000

    Copilot 3rd commit

--- comment.js
+++ comment.js
@@ -1,30 +1,59 @@
-// Create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Parse incoming request with JSON
-app.use(express.json());
-
-// Create a variable to store comments
-const comments = [];
-
-// Create a route to get comments
-app.get('/comments', (req, res) => {
-    res.json(comments);
+//Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+var formidable = require('formidable');
+var util = require('util');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'comment'
 });
-
-// Create a route to post comments
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comments.push(comment);
-    res.json(comment);
+connection.connect();
+//Set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+//Create a table
+var createTable = 'create table if not exists comment (id int primary key auto_increment, name varchar(100), email varchar(100), comment text, date timestamp)';
+connection.query(createTable, function (err, result) {
+    if (err) {
+        console.log('Table creation failed');
+    } else {
+        console.log('Table creation succeeded');
+    }
 });
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+//Create a route
+app.get('/', function (req, res) {
+    var selectData = 'select * from comment order by id desc';
+    connection.query(selectData, function (err, result) {
+        if (err) {
+            console.log('Query failed');
+        } else {
+            res.render('index', {
+                result: result
+            });
+        }
+    });
 });
-
-
-
+app.post('/', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var comment = req.body.comment;
+    var insertData = 'insert into comment (name, email, comment, date) values (?, ?, ?, now())';
+    connection.query(insertData, [name, email, comment], function (err, result) {
+        if (err) {
+            console.log('Insert failed');
+        } else {
+            console.log('Insert succeeded');
+            res.redirect('/');
+        }
+    });
+});
+app.listen(3000);
